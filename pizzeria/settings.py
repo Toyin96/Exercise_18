047ef92f57727f6dff7c11fe71f43a510bc738bd
Toyin96 <onagoruwam@gmail.com>
2021-04-16 00:46:41 +0100

    Pizzeria project updated

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pizzeria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pizzas/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
